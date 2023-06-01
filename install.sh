fb786fc405845fe2019b2a8fac0bd6e9c74b394d
buguno <brunnodesouzabezerra@gmail.com>
2023-05-31 22:33:45 -0300

    ✨ feat: creating install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+PACKAGES=(
+    "git"
+    "tmux"
+    "zsh"
+)
+
+NOT_INSTALLED_PACKAGES=()
+
+for package in ${PACKAGES[@]}; do
+    if ! [ -x "$(command -v $package)" ]; then
+        NOT_INSTALLED_PACKAGES+=($package)
+    fi
+done
+
+if [ ${#NOT_INSTALLED_PACKAGES[@]} -gt 0 ]; then
+    sudo apt update
+    sudo apt install ${NOT_INSTALLED_PACKAGES[@]}
+fi
